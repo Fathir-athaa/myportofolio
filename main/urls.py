ba1f74485be3272c55b85d7819e7bf436b16a0e1
Fathir-athaa <fathir.atha@ui.ac.id>
2026-09-14 03:24:45 +0700

    feat(main): register named route for organization view in urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ app_name = "main"
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
+    path("organization/", show_organization, name="show_organization"),
 ]
